8690ffa3245ca516c33616d4489f4c17141b35d9
LordNedStark <osisiogubenjamin1@gmail.com>
2023-12-22 11:14:44 +0100

    worked on middle line

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-
+  
 import React, { useEffect, useRef } from 'react'
 import './LandingPage.css'
 import {ILiveInYou, nextLevel, PraiseMAchineMusicLogo, faceBookIcon, whatsAppIcon, youtubeIcon, linkedInIcon, manOfWar, kabiyesiOfficialImage} from './landingPageImages'
